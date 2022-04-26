2221b47a0b4e2cca17178dcb74c738ed8acaa650
l00584793 <luozihao3@huawei.com>
2022-04-26 21:24:02 +0800

    Add a test case for adding the plugin syntax to the procedure body.

---
+++ contrib/b_sql_plugin/sql/db_b_plpgsql_test.sql
@@ -0,0 +1,49 @@
+drop database if exists test;
+create database test dbcompatibility 'b';
+\c test
+create extension b_sql_plugin;
+
+create table tb_b_grammar_0038(a text(10)) engine = 表1;
+
+create or replace procedure proc_01()
+as
+begin
+insert tb_b_grammar_0038 values('tom') ;
+end;
+/
+
+create or replace procedure PEOC_165()
+as
+begin
+CREATE TABLE t1 (a binary) engine INNODB;
+end;
+/
+
+CREATE TABLE J1_TBL (
+i integer,
+j integer,
+t text
+);
+
+CREATE TABLE J2_TBL (
+i integer,
+k integer
+);
+INSERT INTO J1_TBL VALUES (1, 4, 'one');
+INSERT INTO J1_TBL VALUES (2, 3, 'two');
+INSERT INTO J1_TBL VALUES (0, NULL, 'zero');
+INSERT INTO J2_TBL VALUES (1, -1);
+INSERT INTO J2_TBL VALUES (NULL, 0);
+
+create or replace procedure PEOC_165()
+as
+begin
+SELECT '' AS "xxx", * FROM J1_TBL JOIN J2_TBL;
+end;
+/
+
+SELECT * from proc_01();
+SELECT * from tb_b_grammar_0038;
+
+\c postgres
+drop database if exists test;
