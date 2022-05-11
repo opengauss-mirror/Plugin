7ce9e362250d963b4f0495a671f41cd2bd6fe69f
kidhasdream <ganyang2@huawei.com>
2022-05-11 16:43:52 +0800

    add fastcheck

---
+++ contrib/b_sql_plugin/sql/empty_value_support_value.sql
@@ -0,0 +1,16 @@
+drop database if exists b;
+create database b dbcompatibility 'b';
+
+\c b
+create extension b_sql_plugin;
+create table test1(num int not null);
+insert into test1 value();
+insert into test1 value(),();
+set sql_mode = '';
+insert into test1 value();
+select * from test1;
+insert into test1 value(),();
+select * from test1;
+
+\c postgres
+drop database if exists b;
