2e2bed9a484e494c7f003d73390a740ad56949b9
zehua.lei <zehua.lei@enmotech.com>
2022-07-22 15:04:56 +0800

    补充测试用例

--- contrib/dolphin/sql/string_func_test/db_b_insert_test.sql
+++ contrib/dolphin/sql/string_func_test/db_b_insert_test.sql
@@ -3,8 +3,15 @@ create database db_b_insert_test dbcompatibility 'B';
 \c db_b_insert_test
 
 select insert('abcdefg', 2, 4, 'yyy');
+select insert(1234567, 2, 4, 'yyy');
 select insert('abcdefg', -100, 4, 'yyy');
 select insert('abcdefg', 100, 4, 'yyy');
+select insert('abcdefg', 2, 200, 'yyy');
+select insert('abcdefg', -100, 200, 'yyy');
+select insert('abcdefg', 100, 200, 'yyy');
+set sql_mode='';
+select insert('abcdefg', '2aa', 4, 'yyy');
+select insert('abcdefg', 'aa2', 4, 'yyy');
 
 \c postgres
 drop database db_b_insert_test;
