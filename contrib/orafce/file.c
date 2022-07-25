719e8eb058798a02f5e8ad07760abb912c17b78c
lvhui <hui.lv@enmotech.com>
2022-07-24 20:16:28 -0700

    在某些特殊场景下，不稳定复现出utl_file中文件句为空的情况，故在此处再次添加对文件名的处理。

--- contrib/orafce/file.c
+++ contrib/orafce/file.c
@@ -901,6 +901,13 @@ get_safe_path(text *location_or_dirname, text *filename)
 	NON_EMPTY_TEXT(location_or_dirname);
 	NON_EMPTY_TEXT(filename);
 
+	if (filename == NULL || (filename != NULL && strlen(filename) == 0)) {
+	    ereport(ERROR, \
+	        (errcode(ERRCODE_INVALID_PARAMETER_VALUE), \
+	        errmsg("invalid parameter"), \
+	        errdetail("Empty string isn't allowed in filename.")));
+	}
+
 	location = safe_named_location(location_or_dirname);
 	if (location)
 	{
