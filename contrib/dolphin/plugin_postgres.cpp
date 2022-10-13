a234b4676f4fe0066ae1c7e9b8d6f0a714969c9d
luozihao <1165977584@qq.com>
2022-10-13 19:46:51 +0800

    支持双引号表示字符串，并新增ansi_quotes的sql_mode进行控制

--- contrib/dolphin/plugin_postgres.cpp
+++ contrib/dolphin/plugin_postgres.cpp
@@ -87,6 +87,7 @@ static const struct sql_mode_entry sql_mode_options[OPT_SQL_MODE_MAX] = {
     {"sql_mode_strict", OPT_SQL_MODE_STRICT},
     {"sql_mode_full_group", OPT_SQL_MODE_FULL_GROUP},
     {"pipes_as_concat", OPT_SQL_MODE_PIPES_AS_CONCAT},
+    {"ansi_quotes", OPT_SQL_MODE_ANSI_QUOTES},
 };
 
 PG_MODULE_MAGIC_PUBLIC;
@@ -332,7 +333,7 @@ void init_session_vars(void)
                                gettext_noop("CUSTOM_OPTIONS"),
                                NULL,
                                &GetSessionContext()->sqlModeString,
-                               "sql_mode_strict,sql_mode_full_group,pipes_as_concat",
+                               "sql_mode_strict,sql_mode_full_group,pipes_as_concat,ansi_quotes",
                                PGC_USERSET,
                                GUC_LIST_INPUT | GUC_REPORT,
                                CheckSqlMode,
