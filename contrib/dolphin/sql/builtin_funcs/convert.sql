e7b80f5063132fe426608ed9e15099c266860b86
zhengjie02 <1282383354@qq.com>
2022-11-23 14:48:46 +0800

    convert支持类型转换

--- contrib/dolphin/sql/builtin_funcs/convert.sql
+++ contrib/dolphin/sql/builtin_funcs/convert.sql
@@ -12,5 +12,7 @@ select convert(1 using 'gbk');
 select convert(1 using 'utf8');
 select convert('测试' using 'utf8');
 select convert('测试' using utf8);
+select convert(11.1, decimal(10,3));
+select convert(1 using decimal(10,3));
 \c postgres
 drop database if exists db_convert;
