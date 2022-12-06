55086c78c16bd8d9792c20cf9d8f41ab46510a98
lvhui <hui.lv@enmotech.com>
2022-12-06 16:27:37 +0800

    add default function test case

--- contrib/dolphin/sql/default_function.sql
+++ contrib/dolphin/sql/default_function.sql
@@ -26,5 +26,15 @@ select default(a.id) from t1 a;
 select default(tt.t3.stime) from tt.t3;
 select default(db_default.tt.t3.id) from tt.t3;
 
+create table t4 (c1 INT NOT NULL AUTO_INCREMENT PRIMARY KEY, c2 VARCHAR(100), c3 VARCHAR(100));
+\d t4
+select default(c1) from t4;
+drop table t4;
+
+create table t5 (c1 int, c2 int, c3 int generated always as (c1-c2) stored);
+insert into t5 values(1, 2);
+select default(c3) from t5;
+drop table t5;
+
 \c postgres
 drop database if exists db_default;
