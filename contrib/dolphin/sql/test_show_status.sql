45d4bc0fec644ea8155e98d131e0de6ead75215a
totaj <totaj@qq.com>
2023-03-07 15:24:07 +0800

    Fix rename user, show processlist, explain and show status bug.

--- contrib/dolphin/sql/test_show_status.sql
+++ contrib/dolphin/sql/test_show_status.sql
@@ -1,6 +1,9 @@
 create schema db_show_status;
 set current_schema to 'db_show_status';
+set dolphin.sql_mode='';
 show status;
+show dolphin.sql_mode;
+reset dolphin.sql_mode;
 
 drop schema db_show_status cascade;
 reset current_schema;
