26975d1bb0d432ec268c2f13de0c7965d29f596b
totaj <totaj@qq.com>
2023-03-11 17:23:13 +0800

    Add flag.

--- contrib/dolphin/plugin_parser/parser.cpp
+++ contrib/dolphin/plugin_parser/parser.cpp
@@ -268,13 +268,17 @@ int base_yylex(YYSTYPE* lvalp, YYLTYPE* llocp, core_yyscan_t yyscanner)
         yyextra->core_yy_extra.is_last_colon = is_last_colon;
     }
 
-    if (cur_token == PARAM && pg_strcasecmp(yyextra->core_yy_extra.scanbuf + *llocp, ":loop") == 0) {
-        cur_token = LABEL_LOOP;
-    } else if (cur_token == PARAM && pg_strcasecmp(yyextra->core_yy_extra.scanbuf + *llocp, ":while") == 0) {
-        cur_token = LABEL_WHILE;
-    } else if (cur_token == PARAM && pg_strcasecmp(yyextra->core_yy_extra.scanbuf + *llocp, ":repeat") == 0) {
-        cur_token = LABEL_REPEAT;
+#ifdef DOLPHIN
+    if (GetSessionContext()->is_first_lable == true && cur_token == PARAM) {
+        if (pg_strcasecmp(yyextra->core_yy_extra.scanbuf + *llocp, ":loop") == 0) {
+            cur_token = LABEL_LOOP;
+        } else if (pg_strcasecmp(yyextra->core_yy_extra.scanbuf + *llocp, ":while") == 0) {
+            cur_token = LABEL_WHILE;
+        } else if (pg_strcasecmp(yyextra->core_yy_extra.scanbuf + *llocp, ":repeat") == 0) {
+            cur_token = LABEL_REPEAT;
+        }
     }
+#endif
 
     /* Do we need to look ahead for a possible multiword token? */
     switch (cur_token) {
