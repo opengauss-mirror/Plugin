766895e8605b59740ee6b42f68f89a891eec080b
chenxiaobin19 <1025221611@qq.com>
2023-03-24 18:21:11 +0800

    修复打开try_vector_engine_strategy后获取char类型长度错误的问题

--- contrib/dolphin/sql/pad_char_to_full_length.sql
+++ contrib/dolphin/sql/pad_char_to_full_length.sql
@@ -34,6 +34,10 @@ select cast('a' as char(10)), 'a'::char(10);
 
 set dolphin.b_compatibility_mode = false;
 
+set try_vector_engine_strategy='force';
+select char_length(name), length(name) from t;
+set try_vector_engine_strategy='off';
+
 set dolphin.sql_mode = '';
 select * from t;
 select concat(name, 't') from t;
@@ -60,6 +64,83 @@ select * from t where name like 'Test        %';
 select * from t where name not like 'Test        %';
 
 select cast('a    ' as char(10)), 'a    '::char(10);
+
+set try_vector_engine_strategy='force';
+select char_length(name), length(name) from t;
+set try_vector_engine_strategy='off';
+
+reset dolphin.sql_mode;
+
+-- test cstore
+create table t_col (name char(100)) with (orientation = column);
+insert into t_col values('test');
+insert into t_col values('test    ');
+insert into t_col values('    test');
+
+select * from t_col;
+select concat(name, 't') from t_col;
+select char_length(name), length(name) from t_col;
+select name::text, name::varchar, name::nvarchar2 from t_col;
+select * from t_col where name = 'test        ';
+select * from t_col where name != 'test        ';
+select * from t_col where name < 'test        ';
+select * from t_col where name <= 'test        ';
+select * from t_col where name > 'test        ';
+select * from t_col where name >= 'test        ';
+select * from t_col where name regexp 'test        %';
+select * from t_col where name like 'test        %';
+select * from t_col where name not like 'test        %';
+select * from t_col where name like 'Test        %';
+select * from t_col where name not like 'Test        %';
+
+set dolphin.b_compatibility_mode = true;
+select * from t_col where name like 'test        %';
+select * from t_col where name not like 'test        %';
+select * from t_col where name like 'Test        %';
+select * from t_col where name not like 'Test        %';
+
+set dolphin.b_compatibility_mode = false;
+
+set try_vector_engine_strategy='force';
+select char_length(name), length(name) from t_col;
+set try_vector_engine_strategy='off';
+
+set enable_codegen to true;
+set codegen_cost_threshold to 0;
+select char_length(name), length(name) from t_col;
+reset enable_codegen;
+reset codegen_cost_threshold;
+
+set dolphin.sql_mode = '';
+select * from t_col;
+select concat(name, 't') from t_col;
+select char_length(name), length(name) from t_col;
+select name::text, name::varchar, name::nvarchar2 from t_col;
+select * from t_col where name = 'test        ';
+select * from t_col where name != 'test        ';
+select * from t_col where name < 'test        ';
+select * from t_col where name <= 'test        ';
+select * from t_col where name > 'test        ';
+select * from t_col where name >= 'test        ';
+select * from t_col where name regexp 'test        %';
+select * from t_col where name like 'test        %';
+select * from t_col where name not like 'test        %';
+select * from t_col where name like 'Test        %';
+select * from t_col where name not like 'Test        %';
+
+set dolphin.b_compatibility_mode = true;
+select * from t_col where name like 'test        %';
+select * from t_col where name not like 'test        %';
+select * from t_col where name like 'Test        %';
+select * from t_col where name not like 'Test        %';
+
+set try_vector_engine_strategy='force';
+select char_length(name), length(name) from t_col;
+set try_vector_engine_strategy='off';
+
+set enable_codegen to true;
+set codegen_cost_threshold to 0;
+select char_length(name), length(name) from t_col;
 
 drop schema pad_char_to_full_length cascade;
 reset current_schema;
