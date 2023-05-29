e2b6998af685904f65110976634cf0ef3db9c76e
shijinpeng5 <shijinpeng5@jd.com>
2023-05-29 18:03:01 +0800

    支持atan(y, x)函数

--- contrib/dolphin/rollback_script/dolphin--1.1--1.0.sql
+++ contrib/dolphin/rollback_script/dolphin--1.1--1.0.sql
@@ -1,3 +1,4 @@
+DROP FUNCTION IF EXISTS pg_catalog.atan (float8, float8);
 DROP FUNCTION IF EXISTS pg_catalog.dolphin_invoke();
 CREATE FUNCTION pg_catalog.dolphin_invoke()
     RETURNS VOID AS '$libdir/dolphin','dolphin_invoke' LANGUAGE C STRICT;
