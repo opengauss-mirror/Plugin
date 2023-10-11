9d214cb78e640f6df42f5f3b9fff5c8f0e673cbc
totaj <totaj@qq.com>
2023-10-11 19:37:25 +0800

    Sync code. 073195ab1f477b5f931b565cb29752a17c41a93e

--- contrib/dolphin/plugin_utility.cpp
+++ contrib/dolphin/plugin_utility.cpp
@@ -645,7 +645,7 @@ void PreventCommandDuringRecovery(const char* cmd_name)
                 errmsg("cannot execute %s during recovery", cmd_name)));
 }
 
-void PreventCommandDuringSSOndemandRecovery(Node* parseTree)
+void PreventCommandDuringSSOndemandRedo(Node* parseTree)
 {
     switch(nodeTag(parseTree)) {
         case T_InsertStmt:
