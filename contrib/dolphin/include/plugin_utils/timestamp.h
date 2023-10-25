a097ebe823e1d7f70f6f968b86f60fb3a89bcfdb
lukeman <lukeman@qq.com>
2023-10-25 16:03:08 +0800

    解决period_add函数同between and使用返回结果有误的问题

--- contrib/dolphin/include/plugin_utils/timestamp.h
+++ contrib/dolphin/include/plugin_utils/timestamp.h
@@ -135,6 +135,8 @@ extern bool datetime_in_with_sql_mode_internal(char *str, struct pg_tm *tm, fsec
 extern bool datetime_in_range(Timestamp datetime);
 extern int128 timestamp_int128(Timestamp timestamp);
 extern int128 timestamptz_int128(TimestampTz timestampTz);
+extern TimestampTz time2timestamptz(TimeADT timeVal);
+extern TimestampTz timetz2timestamptz(TimeTzADT* timetzVal);
 
 extern "C" DLL_PUBLIC Datum int64_b_format_datetime(PG_FUNCTION_ARGS);
 #endif
