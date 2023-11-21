868a3702c4671be250ad80b08cecb8dd18db80b5
08ming <754041231@qq.com>
2023-11-21 09:22:47 +0800

    解决timescaleDB自定义模式名创建超表失败问题

--- contrib/timescaledb/tsl/src/continuous_aggs/create.cpp
+++ contrib/timescaledb/tsl/src/continuous_aggs/create.cpp
@@ -395,7 +395,7 @@ mattablecolumninfo_add_mattable_index(MatTableColumnInfo *matcolinfo, Hypertable
 	IndexStmt stmt = {
 		.type = T_IndexStmt,
 		.missing_ok = false,
-		.schemaname = "public",
+		.schemaname = NameStr(ht->fd.schema_name),
 		.idxname = NULL,
 		.relation = makeRangeVar(NameStr(ht->fd.schema_name), NameStr(ht->fd.table_name), 0),
 		.accessMethod = DEFAULT_INDEX_TYPE,
