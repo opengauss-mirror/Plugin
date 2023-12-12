c83fae8d98bf58a0c94064f54c49e9fa2be69b85
liruixiang <461834084@qq.com>
2023-12-12 23:28:57 +0800

    修复treat_bxconst_as_binary选项下|操作符使用bxconst时报错

--- contrib/dolphin/sql/bxconst_test.sql
+++ contrib/dolphin/sql/bxconst_test.sql
@@ -23,6 +23,32 @@ select * from t_bin;
 drop table t_bit;
 drop table t_bin;
 
+select 0x01 | 2;
+select 0x01 | (2::int1);
+select 0x01 | (2::int2);
+select 0x01 | (2::int4);
+select 0x01 | (2::int8);
+select 0x01 | (2::uint1);
+select 0x01 | (2::uint2);
+select 0x01 | (2::uint4);
+select 0x01 | (2::uint8);
+select 0x01 | (2::float4);
+select 0x01 | (2::float8);
+select 0x01 | (2::numeric);
+select 2 | 0x01;
+select (2::int1) | 0x01;
+select (2::int2) | 0x01;
+select (2::int4) | 0x01;
+select (2::int8) | 0x01;
+select (2::uint1) | 0x01;
+select (2::uint2) | 0x01;
+select (2::uint4) | 0x01;
+select (2::uint8) | 0x01;
+select (2::float4) | 0x01;
+select (2::float8) | 0x01;
+select (2::numeric) | 0x01;
+select 0x01 | 0x02;
+
 -- 将bxconst当作binary处理
 set dolphin.sql_mode = sql_mode_strict,sql_mode_full_group,pipes_as_concat,ansi_quotes,no_zero_date,pad_char_to_full_length,auto_recompile_function,error_for_division_by_zero,treat_bxconst_as_binary;
 
@@ -42,6 +68,32 @@ select * from t_bin;
 
 drop table t_bit;
 drop table t_bin;
+
+select 0x01 | 2;
+select 0x01 | (2::int1);
+select 0x01 | (2::int2);
+select 0x01 | (2::int4);
+select 0x01 | (2::int8);
+select 0x01 | (2::uint1);
+select 0x01 | (2::uint2);
+select 0x01 | (2::uint4);
+select 0x01 | (2::uint8);
+select 0x01 | (2::float4);
+select 0x01 | (2::float8);
+select 0x01 | (2::numeric);
+select 2 | 0x01;
+select (2::int1) | 0x01;
+select (2::int2) | 0x01;
+select (2::int4) | 0x01;
+select (2::int8) | 0x01;
+select (2::uint1) | 0x01;
+select (2::uint2) | 0x01;
+select (2::uint4) | 0x01;
+select (2::uint8) | 0x01;
+select (2::float4) | 0x01;
+select (2::float8) | 0x01;
+select (2::numeric) | 0x01;
+select 0x01 | 0x02;
 
 reset dolphin.sql_mode;
 
