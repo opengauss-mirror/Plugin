c7e569e9b000c592b06b1afea7a7c91e3de9f35b
totaj <totaj@qq.com>
2024-02-20 10:06:14 +0800

    Fix natrual join.

--- contrib/dolphin/plugin_parser/parse_clause.cpp
+++ contrib/dolphin/plugin_parser/parse_clause.cpp
@@ -1400,8 +1400,13 @@ static Node* buildMergedJoinVar(ParseState* pstate, JoinType jointype, Var* l_co
             l_colvar->vartype,
             outcoltype,
             outcoltypmod,
+#ifdef DOLPHIN
+            COERCION_EXPLICIT,
+            COERCE_EXPLICIT_CAST,
+#else
             COERCION_IMPLICIT,
             COERCE_IMPLICIT_CAST,
+#endif
             -1);
     } else if (l_colvar->vartypmod != outcoltypmod) {
         l_node = (Node*)makeRelabelType((Expr*)l_colvar,
@@ -1418,8 +1423,13 @@ static Node* buildMergedJoinVar(ParseState* pstate, JoinType jointype, Var* l_co
             r_colvar->vartype,
             outcoltype,
             outcoltypmod,
+#ifdef DOLPHIN
+            COERCION_EXPLICIT,
+            COERCE_EXPLICIT_CAST,
+#else
             COERCION_IMPLICIT,
             COERCE_IMPLICIT_CAST,
+#endif
             -1);
     } else if (r_colvar->vartypmod != outcoltypmod) {
         r_node = (Node*)makeRelabelType((Expr*)r_colvar,
