80dc5be8b2d2c9408932ccb7391ab1c3fdde5936
wangfeihuo <wangfeihuo@huawei.com>
2024-02-27 21:54:12 +0800

    【标题】: 修复I91D1C所示的json_contains/json_valid/json_length、json_depth、json_storage_size函数返回值类型和mysql不一致的问题 【实现内容】: 修复I91D1C所示的json_contains/json_valid/json_length、json_depth、json_storage_size函数返回值类型和mysql不一致的问题。 【根因分析】: 上述几个函数的返回值类型和mysql不一致。 【实现方案】: 修改上述几个函数的返回值类型，让和mysql一致。 【关联需求或issue】: https://e.gitee.com/opengaussorg/dashboard?issue=I91D1C

--- contrib/dolphin/sql/json_depth.sql
+++ contrib/dolphin/sql/json_depth.sql
@@ -31,5 +31,28 @@ select *from test1;
 select json_depth(data) from test1;
 drop table test1;
 
+set dolphin.b_compatibility_mode = on;
+create table test_json_table
+(
+   `char` char(100),
+   `varchar` varchar(100),
+   `json` json,
+   `int1` tinyint,
+   `binary` binary(100)
+);
+
+insert into test_json_table values ('[1,2,3,4,5]', '[1,2,3,4,5]', json_object('a', 1, 'b', 2), 1, '[1,2,3,4,5]');
+
+select json_depth(`char`), json_depth(`varchar`), json_depth(`json`), pg_typeof(json_depth(`char`)), pg_typeof(json_depth(`varchar`)), pg_typeof(json_depth(`json`)) from test_json_table;
+
+create table test1 as select json_depth(`char`) as c1, json_depth(`varchar`) as c2, json_depth(`json`) as c3 from test_json_table;
+
+-- expect error
+select json_depth(`int1`) from test_json_table;
+select json_depth(`binary`) from test_json_table;
+
+drop table test1;
+drop table test_json_table;
+
 drop schema test_json_depth cascade;
 reset current_schema;
--- contrib/dolphin/sql/json_storage_size.sql
+++ contrib/dolphin/sql/json_storage_size.sql
@@ -71,5 +71,29 @@ FROM
 
 SELECT JSON_STORAGE_SIZE('{0,1}');
 
+
+set dolphin.b_compatibility_mode = on;
+create table test_json_table
+(
+   `char` char(100),
+   `varchar` varchar(100),
+   `json` json,
+   `int1` tinyint,
+   `binary` binary(100)
+);
+
+insert into test_json_table values ('[1,2,3,4,5]', '[1,2,3,4,5]', json_object('a', 1, 'b', 2), 1, '[1,2,3,4,5]');
+
+select json_storage_size(`char`), json_storage_size(`varchar`), json_storage_size(`json`), pg_typeof(json_storage_size(`char`)), pg_typeof(json_storage_size(`varchar`)), pg_typeof(json_storage_size(`json`)) from test_json_table;
+
+create table test1 as select json_storage_size(`char`) as c1, json_storage_size(`varchar`) as c2, json_storage_size(`json`) as c3 from test_json_table;
+
+-- expect error
+select json_storage_size(`int1`) from test_json_table;
+select json_storage_size(`binary`) from test_json_table;
+
+drop table test1;
+drop table test_json_table;
+
 drop schema test_json_storage_size cascade;
 reset current_schema;
