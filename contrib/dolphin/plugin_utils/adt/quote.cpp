d71f199e545cc2941b6088a514a4ba7af218c86b
chenxiaobin19 <1025221611@qq.com>
2024-03-23 16:56:00 +0800

    修复quote的core问题

--- contrib/dolphin/plugin_utils/adt/quote.cpp
+++ contrib/dolphin/plugin_utils/adt/quote.cpp
@@ -62,7 +62,11 @@ static size_t quote_literal_internal(char* dst, const char* src, size_t len)
         if (SQL_STR_DOUBLE(*src, true))
             *dst++ = *src;
         charlen = pg_mblen(src);
-
+#ifdef DOLPHIN
+        if ((size_t)charlen > len) {
+            ereport(ERROR, (errmsg("Cannot convert string 0x%02x to %s", (unsigned char)*src, GetDatabaseEncodingName())));
+        }
+#endif
         for (int i = 0; i < charlen; i++)
             *dst++ = *src++;
 
