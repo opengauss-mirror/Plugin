0b9215d858e47707592fd324d1a525988f27a69c
jiwenke <jwkchn@163.com>
2024-09-04 11:06:58 +0800

    datavec 1.0

--- contrib/datavec/sql/datavec--0.5.1--0.6.0.sql
+++
@@ -1,5 +0,0 @@
--- complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "ALTER EXTENSION datavec UPDATE TO '0.6.0'" to load this file. \quit
-
--- remove this single line for Postgres < 13
-ALTER TYPE datavec SET (STORAGE = external);
