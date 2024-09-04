dfcba38864a976910220911849c8666b10792a83
wangpingyun <2418191738@qq.com>
2024-09-04 14:48:16 +0800

    substring函数第二个参数不支持bit入参

--- contrib/dolphin/upgrade_script/dolphin--3.0--4.0.sql
+++ contrib/dolphin/upgrade_script/dolphin--3.0--4.0.sql
@@ -1241,6 +1241,10 @@ CREATE OR REPLACE FUNCTION pg_catalog.round(timestamp without time zone) RETURNS
 CREATE OR REPLACE FUNCTION pg_catalog.round(anyenum) RETURNS double precision LANGUAGE SQL IMMUTABLE STRICT as 'select pg_catalog.round(cast($1 as double precision))::double precision';
 CREATE OR REPLACE FUNCTION pg_catalog.round(anyset) RETURNS double precision LANGUAGE SQL IMMUTABLE STRICT as 'select pg_catalog.round(cast($1 as double precision))::double precision';
 
+CREATE OR REPLACE FUNCTION pg_catalog.substring(bit, bit) RETURNS bit LANGUAGE SQL IMMUTABLE STRICT as 'select pg_catalog.substring($1, $2::integer)';
+CREATE OR REPLACE FUNCTION pg_catalog.substring(bytea, bit) RETURNS bytea LANGUAGE SQL IMMUTABLE STRICT as 'select pg_catalog.substring($1, $2::integer)';
+CREATE OR REPLACE FUNCTION pg_catalog.substring(text, bit) RETURNS text LANGUAGE SQL IMMUTABLE STRICT as 'select pg_catalog.substring($1, $2::integer)';
+
 drop cast if exists ("binary" as varchar);
 drop cast if exists ("binary" as char);
 CREATE CAST ("binary" AS varchar) WITH FUNCTION pg_catalog.Varlena2Varchar(anyelement) AS IMPLICIT;
