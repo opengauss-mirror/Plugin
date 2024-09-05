a70daa82c419fb61971b6781eba612c1a2bac36b
wei.liu <wei.liu@enmotech.com>
2024-09-05 11:01:15 +0800

    适配最新master版本 orafce插件的编译，确保编译可以成功

--- contrib/orafce/parse_keyword.c
+++ contrib/orafce/parse_keyword.c
@@ -1,18 +1,8 @@
 #include "postgres.h"
 
+#include "parser/scanner.h"
+#include "parser/kwlookup.h"
 #include "parse_keyword.h"
-
-#if PG_VERSION_NUM >= 90600
-
-#include "common/keywords.h"
-
-#else
-
-#include "parser/keywords.h"
-
-#endif
-
-#if PG_VERSION_NUM >= 120000
 
 const char *
 orafce_scan_keyword(const char *text, int *keycode)
@@ -28,22 +18,3 @@ orafce_scan_keyword(const char *text, int *keycode)
 
 	return NULL;
 }
-
-#else
-
-const char *
-orafce_scan_keyword(const char *text, int *keycode)
-{
-	const ScanKeyword *keyword;
-
-	keyword = ScanKeywordLookup(text, ScanKeywords, NumScanKeywords);
-	if (keyword)
-	{
-		*keycode = keyword->value;
-		return keyword->name;
-	}
-
-	return NULL;
-}
-
-#endif
