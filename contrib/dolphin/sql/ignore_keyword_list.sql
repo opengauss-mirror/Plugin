e43b15ab94f1503812fa6d7d500ed248a6704506
wei.liu <wei.liu@enmotech.com>
2024-09-27 10:49:39 +0800

    新增bianry，prior关键字到ignore_keywords， 支持通过设置disable_keyword_options 的方式让binary，prior关键字作为列名使用

--- contrib/dolphin/sql/ignore_keyword_list.sql
+++ contrib/dolphin/sql/ignore_keyword_list.sql
@@ -1,8 +1,6 @@
 create schema ignore_keyword_list;
 set current_schema to 'ignore_keyword_list';
 
-create schema ignore_keyword_list;
-set current_schema to 'ignore_keyword_list';
 select 1 interval; --error
 set disable_keyword_options = 'interval';
 select 1 interval; --ok
@@ -11,7 +9,11 @@ set disable_keyword_options = 'interval,interval';
 select 1 interval; --ok
 reset disable_keyword_options;
 select 1 interval; --error
+create table t1 (binary int);
+create table t2 (prior int);
+set disable_keyword_options ='prior,binary';
+create table t1 (binary int);
+create table t2 (prior int);
+reset current_schema;
 drop schema ignore_keyword_list cascade;
 
-drop schema ignore_keyword_list cascade;
-reset current_schema;
