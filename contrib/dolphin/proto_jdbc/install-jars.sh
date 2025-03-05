ce402afbc8cccdff147e64dfec007686aa1596d0
zhubin79 <18784715772@163.com>
2025-03-05 16:15:02 +0800

    修复门禁jdbc用例

--- contrib/dolphin/proto_jdbc/install-jars.sh
+++ contrib/dolphin/proto_jdbc/install-jars.sh
@@ -3,17 +3,23 @@
 CURRENT_DIR=$(cd `dirname $0`; pwd)
 
 connectorJ="$CURRENT_DIR/class/mysql-connector-java-5.1.47.jar"
+localZip="$CURRENT_DIR/mysql-connector-java-5.1.47.tar.gz"
 if [ ! -f "$connectorJ" ]; then
-     wget http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.47.tar.gz
-     tar -zxf mysql-connector-java-5.1.47.tar.gz 
+     if [ ! -f "$localZip" ]; then
+          wget http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.47.tar.gz 
+     fi
+     tar -zxf $localZip 
      cp mysql-connector-java-5.1.47/mysql-connector-java-5.1.47.jar $CURRENT_DIR/class/
-     rm -rf mysql-connector-java-5.1.47* 
+     rm -rf mysql-connector-java-5.1.47/
 fi
 
 connectorJ="$CURRENT_DIR/class/mysql-connector-java-8.0.28.jar"
+localZip="$CURRENT_DIR/mysql-connector-java-8.0.28.tar.gz"
 if [ ! -f "$connectorJ" ]; then
-     wget http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-8.0.28.tar.gz
-     tar -zxf mysql-connector-java-8.0.28.tar.gz 
+     if [ ! -f "$localZip" ]; then
+          wget http://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-8.0.28.tar.gz
+     fi
+     tar -zxf $localZip 
      cp mysql-connector-java-8.0.28/mysql-connector-java-8.0.28.jar $CURRENT_DIR/class/
-     rm -rf mysql-connector-java-8.0.28* 
+     rm -rf mysql-connector-java-8.0.28/
 fi
