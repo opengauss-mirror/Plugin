5e8696378e8e6ba728987461205a6a4c106b19f2
wangfeihuo <wangfeihuo@huawei.com>
2025-05-21 21:43:36 +0800

    修复tpcc Q33A库和B库执行计划不一样的问题以及补充部分测试用例

--- contrib/dolphin/sql/tpcc_plan.sql
+++ contrib/dolphin/sql/tpcc_plan.sql
@@ -261,14 +261,145 @@ explain (costs off) execute s1 using 1, 2, 3;
 
 
 
+-- with  foreign key and FOREIGN_KEY_CHECKS
+CREATE INDEX idx_customer ON customer (c_w_id,c_d_id,c_last,c_first);
+CREATE INDEX idx_orders ON orders (o_w_id,o_d_id,o_c_id,o_id);
+CREATE INDEX fkey_stock_2 ON stock (s_i_id);
+CREATE INDEX fkey_order_line_2 ON order_line (ol_supply_w_id,ol_i_id);
+
+ALTER TABLE district  ADD CONSTRAINT fkey_district_1 FOREIGN KEY(d_w_id) REFERENCES warehouse(w_id);
+ALTER TABLE customer ADD CONSTRAINT fkey_customer_1 FOREIGN KEY(c_w_id,c_d_id) REFERENCES district(d_w_id,d_id);
+ALTER TABLE history  ADD CONSTRAINT fkey_history_1 FOREIGN KEY(h_c_w_id,h_c_d_id,h_c_id) REFERENCES customer(c_w_id,c_d_id,c_id);
+ALTER TABLE history  ADD CONSTRAINT fkey_history_2 FOREIGN KEY(h_w_id,h_d_id) REFERENCES district(d_w_id,d_id);
+ALTER TABLE new_orders ADD CONSTRAINT fkey_new_orders_1 FOREIGN KEY(no_w_id,no_d_id,no_o_id) REFERENCES orders(o_w_id,o_d_id,o_id);
+ALTER TABLE orders ADD CONSTRAINT fkey_orders_1 FOREIGN KEY(o_w_id,o_d_id,o_c_id) REFERENCES customer(c_w_id,c_d_id,c_id);
+ALTER TABLE order_line ADD CONSTRAINT fkey_order_line_1 FOREIGN KEY(ol_w_id,ol_d_id,ol_o_id) REFERENCES orders(o_w_id,o_d_id,o_id);
+ALTER TABLE order_line ADD CONSTRAINT fkey_order_line_2 FOREIGN KEY(ol_supply_w_id,ol_i_id) REFERENCES stock(s_w_id,s_i_id);
+ALTER TABLE stock ADD CONSTRAINT fkey_stock_1 FOREIGN KEY(s_w_id) REFERENCES warehouse(w_id);
+ALTER TABLE stock ADD CONSTRAINT fkey_stock_2 FOREIGN KEY(s_i_id) REFERENCES item(i_id);
+
+
+set FOREIGN_KEY_CHECKS = 0;
+set enable_beta_opfusion = on;
+set opfusion_debug_mode = 'log';
+
+prepare s1 as SELECT c_discount, c_last, c_credit, w_tax FROM customer, warehouse WHERE w_id = ? AND c_w_id = w_id AND c_d_id = ? AND c_id = ?;
+explain (costs off) execute s1 using 1, 2, 3;
+
+
+prepare s1 as SELECT d_next_o_id, d_tax FROM district WHERE d_id = ? AND d_w_id = ? FOR UPDATE;
+explain (costs off) execute s1 using 1, 2;
+
+prepare s1 as UPDATE district SET d_next_o_id = ? + 1 WHERE d_id = ? AND d_w_id = ?;
+explain (costs off) execute s1 using 1, 2, 3;
+
+prepare s1 as INSERT INTO orders (o_id, o_d_id, o_w_id, o_c_id, o_entry_d, o_ol_cnt, o_all_local) VALUES(?, ?, ?, ?, ?, ?, ?);
+explain (costs off) execute s1 using 1, 2, 3, 4, '2024-10-10 10:10:10', 6, 7;
+
+
+prepare s1 as INSERT INTO new_orders (no_o_id, no_d_id, no_w_id) VALUES (?,?,?);
+explain (costs off) execute s1 using 1, 2, 3;
+
+
+prepare s1 as SELECT i_price, i_name, i_data FROM item WHERE i_id = ?;
+explain (costs off) execute s1 using 1;
+
+prepare s1 as SELECT s_quantity, s_data, s_dist_01, s_dist_02, s_dist_03, s_dist_04, s_dist_05, s_dist_06, s_dist_07, s_dist_08, s_dist_09, s_dist_10 FROM stock WHERE s_i_id = ? AND s_w_id = ? FOR UPDATE;
+explain (costs off) execute s1 using 1, 2;
+
+prepare s1 as UPDATE stock SET s_quantity = ? WHERE s_i_id = ? AND s_w_id = ?;
+explain (costs off) execute s1 using 1, 2, 3;
+
+prepare s1 as INSERT INTO order_line (ol_o_id, ol_d_id, ol_w_id, ol_number, ol_i_id, ol_supply_w_id, ol_quantity, ol_amount, ol_dist_info) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
+explain (costs off) execute s1 using 1, 2, 3, 4, 5, 6, 7, 8, 9;
+
+prepare s1 as UPDATE warehouse SET w_ytd = w_ytd + ? WHERE w_id = ?;
+explain (costs off) execute s1 using 1, 2;
+
+prepare s1 as SELECT w_street_1, w_street_2, w_city, w_state, w_zip, w_name FROM warehouse WHERE w_id = ?;
+explain (costs off) execute s1 using 1;
+
+prepare s1 as UPDATE district SET d_ytd = d_ytd + ? WHERE d_w_id = ? AND d_id = ?;
+explain (costs off) execute s1 using 1, 2, 3;
+
+prepare s1 as SELECT d_street_1, d_street_2, d_city, d_state, d_zip, d_name FROM district WHERE d_w_id = ? AND d_id = ?;
+explain (costs off) execute s1 using 1, 2;
+
+prepare s1 as SELECT count(c_id) FROM customer WHERE c_w_id = ? AND c_d_id = ? AND c_last = ?;
+explain (costs off) execute s1 using 1, 2, 3;
+
+prepare s1 as SELECT c_id FROM customer WHERE c_w_id = ? AND c_d_id = ? AND c_last = ? ORDER BY c_id;
+explain (costs off) execute s1 using 1, 2, 3;
+
+prepare s1 as SELECT c_first, c_middle, c_last, c_street_1, c_street_2, c_city, c_state, c_zip, c_phone, c_credit, c_credit_lim, c_discount, c_balance, c_since FROM customer WHERE c_w_id = ? AND c_d_id = ? AND c_id = ? FOR UPDATE;
+explain (costs off) execute s1 using 1, 2, 3;
+
+prepare s1 as SELECT c_data FROM customer WHERE c_w_id = ? AND c_d_id = ? AND c_id = ?;
+explain (costs off) execute s1 using 1, 2, 3;
+
+prepare s1 as UPDATE customer SET c_balance = ?, c_data = ? WHERE c_w_id = ? AND c_d_id = ? AND c_id = ?;
+explain (costs off) execute s1 using 1, 2, 3, 4, 5;
+
+prepare s1 as UPDATE customer SET c_balance = ? WHERE c_w_id = ? AND c_d_id = ? AND c_id = ?;
+explain (costs off) execute s1 using 1, 2, 3, 4;
+
+prepare s1 as INSERT INTO history(h_c_d_id, h_c_w_id, h_c_id, h_d_id, h_w_id, h_date, h_amount, h_data) VALUES(?, ?, ?, ?, ?, ?, ?, ?);
+explain (costs off) execute s1 using 1, 2, 3, 4, 5, '2024-10-10 10:10:10', 7, 8;
+
+prepare s1 as SELECT count(c_id) FROM customer WHERE c_w_id = ? AND c_d_id = ? AND c_last = ?;
+explain (costs off) execute s1 using 1, 2, 3;
+
+prepare s1 as SELECT c_balance, c_first, c_middle, c_last FROM customer WHERE c_w_id = ? AND c_d_id = ? AND c_last = ? ORDER BY c_first;
+explain (costs off) execute s1 using 1, 2, 3;
+
+prepare s1 as SELECT c_balance, c_first, c_middle, c_last FROM customer WHERE c_w_id = ? AND c_d_id = ? AND c_id = ?;
+explain (costs off) execute s1 using 1, 2, 3;
+
+prepare s1 as SELECT o_id, o_entry_d, COALESCE(o_carrier_id,0) FROM orders WHERE o_w_id = ? AND o_d_id = ? AND o_c_id = ? AND o_id = (SELECT MAX(o_id) FROM orders WHERE o_w_id = ? AND o_d_id = ? AND o_c_id = ?);
+explain (costs off) execute s1 using 1, 2, 3, 4, 5, 6;
+
+prepare s1 as SELECT ol_i_id, ol_supply_w_id, ol_quantity, ol_amount, ol_delivery_d FROM order_line WHERE ol_w_id = ? AND ol_d_id = ? AND ol_o_id = ?;
+explain (costs off) execute s1 using 1, 2, 3;
+
+prepare s1 as SELECT COALESCE(MIN(no_o_id),0) FROM new_orders WHERE no_d_id = ? AND no_w_id = ?;
+explain (costs off) execute s1 using 1, 2;
+
+prepare s1 as DELETE FROM new_orders WHERE no_o_id = ? AND no_d_id = ? AND no_w_id = ?;
+explain (costs off) execute s1 using 1, 2, 3;
+
+prepare s1 as SELECT o_c_id FROM orders WHERE o_id = ? AND o_d_id = ? AND o_w_id = ?;
+explain (costs off) execute s1 using 1, 2, 3;
+
+prepare s1 as UPDATE orders SET o_carrier_id = ? WHERE o_id = ? AND o_d_id = ? AND o_w_id = ?;
+explain (costs off) execute s1 using 1, 2, 3, 4;
+
+prepare s1 as UPDATE order_line SET ol_delivery_d = ? WHERE ol_o_id = ? AND ol_d_id = ? AND ol_w_id = ?;
+explain (costs off) execute s1 using '2024-10-10 10:10:10', 2, 3, 4;
+
+prepare s1 as SELECT SUM(ol_amount) FROM order_line WHERE ol_o_id = ? AND ol_d_id = ? AND ol_w_id = ?;
+explain (costs off) execute s1 using 1, 2, 3;
+
+prepare s1 as UPDATE customer SET c_balance = c_balance + ? , c_delivery_cnt = c_delivery_cnt + 1 WHERE c_id = ? AND c_d_id = ? AND c_w_id = ?;
+explain (costs off) execute s1 using 1, 2, 3, 4;
+
+prepare s1 as SELECT d_next_o_id FROM district WHERE d_id = ? AND d_w_id = ?;
+explain (costs off) execute s1 using 1, 2;
+
+prepare s1 as SELECT DISTINCT ol_i_id FROM order_line WHERE ol_w_id = ? AND ol_d_id = ? AND ol_o_id < ? AND ol_o_id >= (? - 20);
+explain (costs off) execute s1 using 1, 2, 3, 4;
+
+prepare s1 as SELECT count(*) FROM stock WHERE s_w_id = ? AND s_i_id = ? AND s_quantity < ?;
+explain (costs off) execute s1 using 1, 2, 3;
+
+
+drop table history;
+drop table order_line;
+drop table new_orders;
+drop table orders;
 drop table customer;  
 drop table district; 
-drop table history;
-drop table item;
-drop table new_orders;
-drop table order_line;
-drop table orders;
 drop table stock;
+drop table item;
 drop table warehouse;
 
 drop schema tpcc_plan cascade;
