909b41eb22776f9d9dc83b86fbdfbe15304391de
totaj <totaj@qq.com>
2025-05-24 11:12:11 +0800

    Use execute_parse_message/execute_bind_message/execute_exec_message for mysql pbe protocol.

--- contrib/dolphin/plugin_protocol/handler.cpp
+++ contrib/dolphin/plugin_protocol/handler.cpp
@@ -383,43 +383,24 @@ int execute_text_protocol_sql(const char *sql)
 
 int execute_com_stmt_prepare(const char *client_sql)
 {
-    int rc;
+    CachedPlanSource *psrc = NULL;
+    PreparedStatement *pstmt = NULL;
+    int32 statement_id = gs_atomic_add_32(&g_proto_ctx.statement_id, 1);
     char stmt_name[NAMEDATALEN];
-    
-    start_xact_command();
+    int rc = sprintf_s(stmt_name, NAMEDATALEN, "%s%d", DOLPHIN_PROTOCOL_STMT_NAME_PREFIX, statement_id);
+    securec_check_ss(rc, "", "");
 
-    if (!u_sess->attr.attr_storage.phony_autocommit) {
-        BeginTxnForAutoCommitOff();
-    }
-
-    SPI_STACK_LOG("connect", NULL, NULL);
-    if ((rc = SPI_connect()) != SPI_OK_CONNECT) {
-        ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR),
-            errmsg("dolphin SPI_connect failed: %s", SPI_result_code_string(rc)),
-            errdetail("SPI_connect failed"),
-            errcause("System error."),
-            erraction("Check whether the snapshot retry is successful")));
-    }
+    exec_parse_message(client_sql, stmt_name, NULL, NULL, NULL, 0, false);
+    get_prepared_statement(stmt_name, &pstmt, &psrc);
+    int column_count = psrc->resultDesc == NULL ? 0 : psrc->resultDesc->natts;
+    int param_count = psrc->num_params;
 
     StringInfo sql = makeStringInfo();
-    int32 statement_id = gs_atomic_add_32(&g_proto_ctx.statement_id, 1);
-    rc = snprintf_s(stmt_name, NAMEDATALEN + 1, NAMEDATALEN, "p%d", statement_id);
-    securec_check_ss(rc, "\0", "\0");
-
-    appendStringInfo(sql, "prepare %s from '%s'", stmt_name, client_sql);
-
-    rc = SPI_execute(sql->data, false, 0);
-
-    PreparedStatement *pstmt = FetchPreparedStatement(stmt_name, true, true);
-    int column_count = pstmt->plansource->resultDesc == NULL ? 0 : pstmt->plansource->resultDesc->natts;
-    int param_count = pstmt->plansource->num_params;
-
-    resetStringInfo(sql);
     send_com_stmt_prepare_ok_packet(sql, statement_id, column_count, param_count);
 
     for (int i = 0; i < param_count; i++) {
         dolphin_column_definition* param_field = make_dolphin_column_definition("?");
-        const TypeItem* item = GetItemByTypeOid(pstmt->plansource->param_types[i]);
+        const TypeItem* item = GetItemByTypeOid(psrc->param_types[i]);
         param_field->type = item->dolphin_type_id;
         param_field->charsetnr = item->charset_flag;
         send_column_definition41_packet(sql, param_field);
@@ -436,70 +417,168 @@ int execute_com_stmt_prepare(const char *client_sql)
         send_network_eof_packet(sql);
     }
     DestroyStringInfo(sql);
-
-    SPI_STACK_LOG("finish", NULL, NULL);
-    SPI_finish();
-    finish_xact_command();
-
     return 0;
 }
 
-int execute_binary_protocol_req(com_stmt_exec_request *request)
+void dolphin_get_param_list_info(PqBindMessage* pqBindMessage, CachedPlanSource* psrc, ParamListInfo* params)
 {
-    StringInfo sql = makeStringInfo();
-    appendStringInfo(sql, "execute p%d using ", request->statement_id);
+    int numParams = pqBindMessage->numParams;
+    /*
+     * Fetch parameters, if any, and store in the portal's memory context.
+     */
+    if (numParams <= 0) {
+        return;
+    }
 
-    for (uint i = 0; i < request->param_count; i++) {
-        switch (request->parameter_values[i].type) {
+    if (*params == NULL) {
+        *params = (ParamListInfo)palloc(offsetof(ParamListInfoData, params) + numParams * sizeof(ParamExternData));
+        /* we have static list of params, so no hooks needed */
+        (*params)->paramFetch = NULL;
+        (*params)->paramFetchArg = NULL;
+        (*params)->parserSetup = NULL;
+        (*params)->parserSetupArg = NULL;
+        (*params)->params_need_process = false;
+        (*params)->numParams = numParams;
+        (*params)->uParamInfo = DEFUALT_INFO;
+        (*params)->params_lazy_bind = false;
+    }
+    com_stmt_param *param = (com_stmt_param *)pqBindMessage->pvalue;
+
+    for (int i = 0; i < numParams; i++) {
+        bool isNull = false;
+        Datum pval = 0;
+        Oid ptype = psrc->param_types[i];
+        Oid sendType = InvalidOid;
+        switch (param[i].type) {
             case TYPE_STRING: {
-                if (request->parameter_values[i].value.text) {
-                    appendStringInfo(sql, "'%s'", request->parameter_values[i].value.text);
+                if (param[i].value.text) {
+                    Oid typinput;
+                    Oid typioparam;
+                    getTypeInputInfo(ptype, &typinput, &typioparam);
+                    pval = OidInputFunctionCall(typinput, (char*)param[i].value.text, typioparam, -1);
                 } else {
-                    appendStringInfo(sql, "(null)");
+                    isNull = true;
                 }
-                
                 break;
             }
             case TYPE_INT1: {
-                appendStringInfo(sql, "%d", request->parameter_values[i].value.i1);
+                sendType = INT1OID;
+                pval = Int8GetDatum(param[i].value.i1);
                 break;
             }
             case TYPE_INT2:
             case TYPE_INT4: {
-                appendStringInfo(sql, "%d", request->parameter_values[i].value.i4);
+                sendType = INT4OID;
+                pval = Int32GetDatum(param[i].value.i4);
                 break;
             }
             case TYPE_INT8: {
-                appendStringInfo(sql, "%lld", (int64)(request->parameter_values[i].value.i8));
+                sendType = INT8OID;
+                pval = Int64GetDatum(param[i].value.i8);
                 break;
             }
             case TYPE_FLOAT: {
-                appendStringInfo(sql, "%f", request->parameter_values[i].value.f.f4);
+                sendType = FLOAT4OID;
+                pval = Float4GetDatum(param[i].value.f.f4);
                 break;
             }
             case TYPE_DOUBLE: {
-                appendStringInfo(sql, "%lf", request->parameter_values[i].value.d.f8);
+                sendType = FLOAT8OID;
+                pval = Float8GetDatum(param[i].value.d.f8);
                 break;
             }
             case TYPE_HEX: {
+                size_t value_len = strlen(param[i].value.text);
+                StringInfo sql = makeStringInfo();
                 appendStringInfo(sql, "x'");
-                for (uint j = 0; j < strlen(request->parameter_values[i].value.text); j++) {
-                    appendStringInfo(sql, "%x", request->parameter_values[i].value.text[j]);
+                for (size_t j = 0; j < value_len; j++) {
+                    appendStringInfo(sql, "%x", param[i].value.text[j]);
                 }
                 appendStringInfo(sql, "'");
+                Oid typinput;
+                Oid typioparam;
+                getTypeInputInfo(VARBITOID, &typinput, &typioparam);
+                pval = OidInputFunctionCall(typinput, (char*)sql->data, typioparam, -1);
+                DestroyStringInfo(sql);
                 break;
             }
             default:
+                isNull = true;
                 break;
         }
-        
-        if (i < request->param_count - 1) {
-            appendStringInfoString(sql, ",");
+
+        if (!isNull && sendType != InvalidOid && sendType != ptype) {
+            /* maybe we have better way to deal with these data type transform? */
+            Oid castFunc;
+            /*
+             * actually we can use COERCION_EXPLICIT here, but in find_coercion_pathway, COERCION_EXPLICIT will do many
+             * other things we don't want, so just give COERCION_EXPLICIT, it's seems ok now.
+             */
+            CoercionPathType pathtype = find_coercion_pathway(ptype, sendType, COERCION_ASSIGNMENT, &castFunc);
+            switch (pathtype) {
+                case COERCION_PATH_NONE:
+                    ereport(ERROR, (errmsg("could not cast source type: %d to target type: %d.", sendType, ptype)));
+                    break;
+                case COERCION_PATH_FUNC:
+                    pval = OidFunctionCall1(castFunc, pval);
+                    break;
+                case COERCION_PATH_RELABELTYPE:
+                    /* binary equal, noting to do */
+                    break;
+                case COERCION_PATH_COERCEVIAIO:
+                case COERCION_PATH_ARRAYCOERCE: {
+                    Oid typinout;
+                    Oid typioparam;
+                    bool typIsVarlena = false;
+
+                    getTypeOutputInfo(sendType, &typinout, &typIsVarlena);
+                    char* result = OidOutputFunctionCall(typinout, pval);
+
+                    getTypeInputInfo(ptype, &typinout, &typioparam);
+                    pval = OidInputFunctionCall(typinout, result, typioparam, -1);
+                    pfree_ext(result);
+                    break;
+                }
+                default:
+                    /* should not happen */
+                    ereport(ERROR, (errmsg("Unknown coerce path type: %d.", pathtype)));
+                    break;
+            }
         }
+        (*params)->params[i].value = pval;
+        (*params)->params[i].isnull = isNull;
+
+        /*
+         * We mark the params as CONST.  This ensures that any custom plan
+         * makes full use of the parameter values.
+         */
+        (*params)->params[i].pflags = PARAM_FLAG_CONST;
+        (*params)->params[i].ptype = ptype;
+        (*params)->params[i].tabInfo = NULL;
     }
+}
 
-    execute_simple_query(sql->data);
+int execute_binary_protocol_req(com_stmt_exec_request *request)
+{
+    char stmt_name[NAMEDATALEN];
+    int rc = sprintf_s(stmt_name, NAMEDATALEN, "%s%d", DOLPHIN_PROTOCOL_STMT_NAME_PREFIX, request->statement_id);
+    securec_check_ss(rc, "", "");
+    PqBindMessage pqBindMessage;
+    pqBindMessage.portalName = "";
+    pqBindMessage.stmtName = stmt_name;
+    pqBindMessage.numPFormats = 0;
+    pqBindMessage.pformats = NULL;
+    pqBindMessage.numParams = request->param_count;
+    pqBindMessage.plength = NULL;
+    pqBindMessage.pvalue = (const char**)request->parameter_values;
+    pqBindMessage.numRFormats = 0;
+    pqBindMessage.rformats = NULL;
 
+    /* set use_parame to true, so in opfusion state it will send row desc, check OpFusion::setReceiver */
+    u_sess->param_cxt.use_parame = true;
+    exec_bind_message(&pqBindMessage, NULL, NULL, false, dolphin_get_param_list_info);
+    exec_execute_message(pqBindMessage.portalName, FETCH_ALL, false);
+    u_sess->param_cxt.use_parame = false;
     return 0;
 }
 
@@ -539,29 +618,10 @@ void execute_com_stmt_close(StringInfo buf)
     uint32 statement_id;
     char stmt_name[NAMEDATALEN];
     dq_get_int4(buf, &statement_id);
-    int rc = snprintf_s(stmt_name, NAMEDATALEN + 1, NAMEDATALEN, "p%d", statement_id);
+    int rc = sprintf_s(stmt_name, NAMEDATALEN, "%s%d", DOLPHIN_PROTOCOL_STMT_NAME_PREFIX, statement_id);
     securec_check_ss(rc, "\0", "\0");
 
-    StringInfo sql = makeStringInfo();
-    appendStringInfo(sql, "DEALLOCATE %s", stmt_name);
-
-    start_xact_command();
-
-    SPI_STACK_LOG("connect", NULL, NULL);
-    if ((rc = SPI_connect()) != SPI_OK_CONNECT) {
-        ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR),
-            errmsg("dolphin SPI_connect failed: %s", SPI_result_code_string(rc)),
-            errdetail("SPI_connect failed"),
-            errcause("System error."),
-            erraction("Check whether the snapshot retry is successful")));
-    }
-
-    rc = SPI_execute(sql->data, false, 0);
-
-    DestroyStringInfo(sql);
-    SPI_STACK_LOG("finish", NULL, NULL);
-    SPI_finish();
-    finish_xact_command();
+    DropPreparedStatement(stmt_name, false);
 
     remove_cached_stmt_data(&statement_id);
 }
