7077b90a2f58d597817fda3a43046fd60cbae0dc
wangfeihuo <wangfeihuo@huawei.com>
2025-07-10 17:11:24 +0800

    修复create table as的时候包含date_sub时报错的问题

--- contrib/dolphin/sql/date_add_time_interval.sql
+++ contrib/dolphin/sql/date_add_time_interval.sql
@@ -21,5 +21,13 @@ select date_sub(time'838:59:59', interval 1 month);
 select date_add(time'838:59:59', interval -1 year);
 select date_sub(time'838:59:59', interval 1 year);
 
+create table test1(c1 date);
+create table x1 as select * from test1 where c1 >= date_sub(curdate(), interval 3 month);
+create table x2 as select * from test1 where c1 >= date_add(curdate(), interval 3 month);
+
+drop table test1;
+drop table x1;
+drop table x2;
+
 reset current_schema;
 drop schema date_add_time_interval;
