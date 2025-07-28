cb9ed2394320e5324d5591f7135d6240cae8dd44
totaj <totaj@qq.com>
2025-07-28 21:12:09 +0800

    phread mutex to rwlock

--- contrib/dolphin/plugin_optimizer/commands/functioncmds.cpp
+++ contrib/dolphin/plugin_optimizer/commands/functioncmds.cpp
@@ -1770,8 +1770,8 @@ void libraryDoPendingDeletes(bool isCommit)
      * protect process variable file_list and CFuncHash, avoid they be
      * concurrent changed of different thread.
      */
-    AutoMutexLock libraryLock(&dlerror_lock);
-    libraryLock.lock();
+    AutoRWLock libraryLock(&g_dlerror_lock_rw);
+    libraryLock.WrLock();
 
     ListCell* lc = NULL;
     foreach (lc, u_sess->cmd_cxt.PendingLibraryDeletes) {
@@ -1788,7 +1788,7 @@ void libraryDoPendingDeletes(bool isCommit)
 
     /* Reset PendingLibraryDeletes.*/
     ResetPendingLibraryDelete();
-    libraryLock.unLock();
+    libraryLock.UnLock();
 }
 
 /*
