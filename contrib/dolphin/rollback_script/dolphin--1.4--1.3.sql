33166e79dbb184d37bb21bcb94777685e23c890a
shijuzheng1997 <shijuzheng1997@126.com>
2025-07-28 21:30:40 +0800

    修复date_add(timestamp without time zone, interval)情况下输出丢失精度的问题

--- contrib/dolphin/rollback_script/dolphin--1.4--1.3.sql
+++ contrib/dolphin/rollback_script/dolphin--1.4--1.3.sql
@@ -1,2 +1,5 @@
 DROP FUNCTION IF EXISTS pg_catalog.mod(double precision, int);
 DROP FUNCTION IF EXISTS pg_catalog.mod(int, double precision);
+
+DROP FUNCTION IF EXISTS pg_catalog.date_add (timestamp without time zone, interval);
+DROP FUNCTION IF EXISTS pg_catalog.date_sub(timestamp without time zone, interval);
--- contrib/dolphin/upgrade_script/dolphin--1.3--1.4.sql
+++ contrib/dolphin/upgrade_script/dolphin--1.3--1.4.sql
@@ -1,2 +1,7 @@
 CREATE OR REPLACE FUNCTION pg_catalog.mod(double precision, int) RETURNS number LANGUAGE SQL IMMUTABLE STRICT as 'select pg_catalog.mod(cast($1 as number), cast($2 as number))';
 CREATE OR REPLACE FUNCTION pg_catalog.mod(int, double precision) RETURNS number LANGUAGE SQL IMMUTABLE STRICT as 'select pg_catalog.mod(cast($1 as number), cast($2 as number))';
+
+DROP FUNCTION IF EXISTS pg_catalog.date_add(timestamp without time zone, interval);
+CREATE OR REPLACE FUNCTION pg_catalog.date_add(timestamp without time zone, interval) RETURNS text AS $$ SELECT pg_catalog.adddate($1::text, $2)  $$ LANGUAGE SQL;
+DROP FUNCTION IF EXISTS pg_catalog.date_sub(timestamp without time zone, interval);
+CREATE OR REPLACE FUNCTION pg_catalog.date_sub(timestamp without time zone, interval) RETURNS text AS $$ SELECT pg_catalog.adddate($1::text, -$2)  $$ LANGUAGE SQL;
