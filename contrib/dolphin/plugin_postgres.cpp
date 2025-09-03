3dffd0208003b1f1e7245788ada7da1a21bbf3b2
totaj <totaj@qq.com>
2025-09-03 11:59:54 +0800

    optimize getsessioncontext and b protocol simple query output

--- contrib/dolphin/plugin_postgres.cpp
+++ contrib/dolphin/plugin_postgres.cpp
@@ -889,7 +889,7 @@ BSqlPluginContext* GetSessionContext()
     if (u_sess->attr.attr_common.extension_session_vars_array[dolphin_index] == NULL) {
         init_session_vars();
     }
-    return (BSqlPluginContext *) u_sess->attr.attr_common.extension_session_vars_array[dolphin_index];
+    return (BSqlPluginContext *)u_sess->attr.attr_common.extension_session_vars_array[dolphin_index];
 }
 
 void set_extension_index(uint32 index)
