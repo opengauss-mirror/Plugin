be830f226ba1ce15e2680fa8bf824db4acb5773e
wangfeihuo <wangfeihuo@huawei.com>
2026-01-24 17:13:25 +0800

    修复db_b_format的问题

--- contrib/dolphin/rollback_script/dolphin--5.2--5.1.sql
+++ contrib/dolphin/rollback_script/dolphin--5.2--5.1.sql
@@ -4,3 +4,4 @@ DROP FUNCTION IF EXISTS pg_catalog.varchar_bytea(varchar);
 DROP VIEW IF EXISTS performance_schema.threads;
 
 DROP FUNCTION IF EXISTS pg_catalog.db_b_format(number, name);
+DROP FUNCTION IF EXISTS pg_catalog.db_b_format(unknown, unknown);
