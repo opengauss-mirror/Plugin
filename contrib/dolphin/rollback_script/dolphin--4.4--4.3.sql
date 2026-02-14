02e0d0601e6c4178b81fc414d94e34d37e5732d5
dodders <laishenghao1@huawei.com>
2026-02-14 09:22:55 +0800

    版本升级与同步yearweek兼容性函数

---
+++ contrib/dolphin/rollback_script/dolphin--4.4--4.3.sql
@@ -0,0 +1,4 @@
+drop function if exists pg_catalog.yearweek(timestamp without time zone);
+drop function if exists pg_catalog.yearweek(timestamp with time zone);
+drop function if exists pg_catalog.yearweek(timestamp without time zone, int8);
+drop function if exists pg_catalog.yearweek(timestamp with time zone, int8);
