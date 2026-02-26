d6bdbd511352562d8b6425188c0013317e89dcb7
wangfeihuo <wangfeihuo@huawei.com>
2026-02-26 10:15:03 +0800

    修复ExecuteCallStmt参数没传oid的问题

--- contrib/dolphin/plugin_optimizer/commands/functioncmds.cpp
+++ contrib/dolphin/plugin_optimizer/commands/functioncmds.cpp
@@ -124,6 +124,8 @@ static void CheckInternalParamsReturnType(Oid declaredRetOid, Oid languageOid,
     List* asClause, oidvector* parameterTypes, bool* isStrict);
 static bool IsTypeMatch(Oid oid1, Oid oid2);
 static void pipelined_function_sanity_check(const CreateFunctionStmt *stmt, bool isPipelined);
+extern void extract_charset_collid_from_setstmt(VariableSetStmt* setstmt, char** charsetOut, char** collationOut);
+
 static void CreateFunctionComment(Oid funcOid, List* options, bool lock = false)
 {
     ListCell *cell = NULL;
@@ -730,9 +732,23 @@ static ArrayType* update_proconfig_value(ArrayType* a, const List* set_items)
         VariableSetStmt* sstmt = (VariableSetStmt*)lfirst(l);
 
         Assert(IsA(sstmt, VariableSetStmt));
-        if (sstmt->kind == VAR_RESET_ALL)
+        if (sstmt->kind == VAR_RESET_ALL) {
             a = NULL;
-        else {
+        } else if (ENABLE_MULTI_CHARSET && strcmp(sstmt->name, "set_names") == 0) {
+            char* valuestr = ExtractSetVariableArgs(sstmt);
+            if (valuestr != NULL) {
+                char* charset;
+                char* collation;
+                extract_charset_collid_from_setstmt(sstmt, &charset, &collation);
+                a = GUCArrayAdd(a, "client_encoding", charset);
+                a = GUCArrayAdd(a, "character_set_connection", charset);
+                a = GUCArrayAdd(a, "collation_connection", collation);
+            } else {
+                a = GUCArrayDelete(a, "client_encoding");
+                a = GUCArrayDelete(a, "character_set_connection");
+                a = GUCArrayDelete(a, "collation_connection");
+            }
+        } else {
             char* valuestr = ExtractSetVariableArgs(sstmt);
 
             if (valuestr != NULL)
@@ -4351,6 +4367,7 @@ void ExecuteCallStmt(DolphinCallStmt *stmt, ParamListInfo params, bool atomic, D
 
         fcinfo.arg[i] = val;
         fcinfo.argnull[i] = isnull;
+        fcinfo.argTypes[i] = exprstate->resultType;
 
         i++;
     }
