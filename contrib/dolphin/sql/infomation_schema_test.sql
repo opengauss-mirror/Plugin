8034c93db565f42001baddc72c7f7f59638a7c72
wofanzheng <2399541971@qq.com>
2026-06-27 15:19:37 +0800

    适配2394回合到6.0.0测试布局

--- contrib/dolphin/sql/infomation_schema_test.sql
+++ contrib/dolphin/sql/infomation_schema_test.sql
@@ -19,6 +19,56 @@ select * from information_schema.columns where table_schema = 'infomation_schema
 select * from information_schema.columns where table_schema = 'infomation_schema_test' and table_name = 'employees3' and column_name = 'emp_name';
 select * from information_schema.columns where table_schema = 'infomation_schema_test' and table_name = 'employees4' and column_name = 'emp_time';
 
+drop table if exists having_alias_info_schema_test;
+create table having_alias_info_schema_test(id int);
+
+select
+    table_schema as table_cat,
+    null as table_schem,
+    table_name,
+    case
+        when table_type = 'BASE TABLE' then 'TABLE'
+        else table_type
+    end as table_type
+from information_schema.tables
+where table_schema = 'infomation_schema_test'
+    and table_name = 'having_alias_info_schema_test'
+group by table_schema, table_name, information_schema.tables.table_type
+having table_type in ('TABLE', null)
+order by table_type, table_schema, table_name;
+
+drop table if exists having_alias_grouping_test;
+create table having_alias_grouping_test(a int, b int);
+insert into having_alias_grouping_test values (1, 10), (2, 20);
+select a + 1 as a, sum(b)
+from having_alias_grouping_test
+group by rollup(a)
+having grouping(a) = 0
+order by 1;
+drop table having_alias_grouping_test;
+
+drop table if exists having_alias_outer_test;
+drop table if exists having_alias_inner_test;
+create table having_alias_outer_test(id int);
+create table having_alias_inner_test(id int);
+insert into having_alias_outer_test values (1);
+insert into having_alias_inner_test values (1), (2);
+select count(*)
+from (
+    select id as id
+    from having_alias_outer_test o
+    where exists (
+        select 1
+        from having_alias_inner_test i
+        group by i.id
+        having id > 1
+    )
+) s;
+drop table having_alias_outer_test;
+drop table having_alias_inner_test;
+
+drop table having_alias_info_schema_test;
+
 drop table employees;
 drop table employees2;
 drop table employees3;
@@ -26,4 +76,3 @@ drop table employees4;
 
 reset current_schema;
 drop schema infomation_schema_test;
-
