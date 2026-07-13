4b90eb8fa15798abec58a137653d6319075cfd45
liyangx <18352857663@163.com>
2026-07-13 20:46:44 +0800

    fix: handle uuid format in dolphin

--- contrib/dolphin/rollback_script/dolphin--5.3--5.2.sql
+++ contrib/dolphin/rollback_script/dolphin--5.3--5.2.sql
@@ -4,6 +4,11 @@ DROP FUNCTION IF EXISTS pg_catalog.date_format (timestamp with time zone, text);
 CREATE OR REPLACE FUNCTION pg_catalog.date_format (timestamp with time zone, text) RETURNS TEXT LANGUAGE SQL STABLE STRICT as  $$ SELECT pg_catalog.date_format($1::text, $2); $$ ;
 
 CREATE OR REPLACE FUNCTION pg_catalog.db_b_format(number, name) RETURNS text LANGUAGE SQL IMMUTABLE STRICT as 'select db_b_format($1::"any", $2::bigint)';
+DROP FUNCTION IF EXISTS pg_catalog.db_b_format(varchar, varchar);
+DROP FUNCTION IF EXISTS pg_catalog.db_b_format(unknown, int4);
+DROP FUNCTION IF EXISTS pg_catalog.db_b_format(unknown, number);
+DROP FUNCTION IF EXISTS pg_catalog.db_b_format(unknown, int8);
+DROP FUNCTION IF EXISTS pg_catalog.db_b_format(number, number);
 
 DROP OPERATOR IF EXISTS pg_catalog.<>(text, binary);
 DROP OPERATOR IF EXISTS pg_catalog.<>(binary, text);
