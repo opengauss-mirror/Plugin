38047699d73e4b276dfaf660d041c6d254a12d45
liyangx <18352857663@163.com>
2026-09-09 09:24:50 +0800

    update: hh 49 package source exit handing

--- contrib/dolphin/plugin_pl/plpgsql/src/pl_handler.cpp
+++ contrib/dolphin/plugin_pl/plpgsql/src/pl_handler.cpp
@@ -196,6 +196,10 @@ static void processError(CreateFunctionStmt* stmt, enum FunctionErrorType ErrorT
 
 void InsertGsSource(Oid objId, Oid nspid, const char* name, const char* type, bool status)
 {
+    if (g_instance.status != NoShutdown ||
+        t_thrd.proc_cxt.proc_exit_inprogress) {
+        return;
+    }
     bool notInsert = u_sess->attr.attr_common.upgrade_mode != 0 ||
         OidIsValid(u_sess->cmd_cxt.CurrentExtensionObject) ||
         IsSystemNamespace(nspid) || IsToastNamespace(nspid) ||
